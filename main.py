50734dc00729178901556865798dfa1804f9eca3
UjjwalThapliyal123 <ujjwalthapliyal4@gmail.com>
2026-09-09 10:18:36 +0530

    PDF

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI, UploadFile, File
+from pdf2image import convert_from_bytes
+import pytesseract
+
+app = FastAPI()
+
+@app.post("/ocr")
+async def extract_pdf_text(file: UploadFile = File(...)):
+    contents = await file.read()
+    # Convert PDF pages into PIL image buffers
+    images = convert_from_bytes(contents)
+    
+    extracted_text = ""
+    for img in images:
+        extracted_text += pytesseract.image_to_string(img) + "\n"
+        
+    return {"text": extracted_text}
+    
